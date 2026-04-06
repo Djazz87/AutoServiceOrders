2c9e96360b168e6018a00f61cc6274712f45bc1b
user <1@mail.ru>
2026-04-06 12:01:22 +1000

    ss

--- AutoService Order/ViewModels/WorkWindowViewModel.cs
+++ AutoService Order/ViewModels/WorkWindowViewModel.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoService_Order.DB;
 using AutoService_Order.Models;
+using AutoService_Order.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AutoService_Order.ViewModels;
 
@@ -15,22 +19,30 @@ public partial class WorkWindowViewModel : ViewModelBase
     private readonly WorkRepository _workRepository;
     private readonly ServicesRepository _serviceRepository;
     private readonly Services _services;
+    private readonly IServiceProvider _provider;
+
   
    
 
    [ObservableProperty] private List<SelectedWorks> _works;
 
-   public WorkWindowViewModel(WorkRepository workRepository, ServicesRepository serviceRepository, Services services)
+   public WorkWindowViewModel(IServiceProvider provider,  WorkRepository workRepository, ServicesRepository serviceRepository, Services services)
    {
+       _provider = provider;
        _workRepository = workRepository;
        _serviceRepository = serviceRepository;
        Works = workRepository.GetWorks(services).Select(s=>new SelectedWorks{ Works = s}).ToList();
        Title = services.Title;
        _services = services;
+      
    }
 
+   [RelayCommand]
    public void GetItog()
    {
-       
+       var vm = ActivatorUtilities.CreateInstance<OrderWindowVIewModel>(_provider, Price, Works);
+       var win = _provider.GetService<OrderWindow>();
+       win.DataContext = vm;
+       win.Show();
    }
 }
